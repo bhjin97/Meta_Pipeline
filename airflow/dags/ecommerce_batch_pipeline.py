2f818a35a31943837c6c8210bb42525c96dde8c9
bhjin97 <baehj0907@gmail.com>
2026-05-09 13:35:52 +0000

    bash to sparksubmit

--- airflow/dags/ecommerce_batch_pipeline.py
+++ airflow/dags/ecommerce_batch_pipeline.py
@@ -1,74 +1,78 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.utils.task_group import TaskGroup
 
 
 SPARK_PACKAGES = (
     "org.apache.hadoop:hadoop-aws:3.3.4,"
-    "com.amazonaws:aws-java-sdk-bundle:1.12.262"
+    "com.amazonaws:aws-java-sdk-bundle:1.12.262,"
+    "org.postgresql:postgresql:42.7.3"
 )
 
-BASE_CMD = """
-spark-submit \
-  --master spark://spark-master:7077 \
-  --packages {packages} \
-  /app/spark/batch/{script}
-"""
+default_args = {
+    "owner": "airflow",
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+    "execution_timeout": timedelta(minutes=30),
+}
 
 
 with DAG(
-    dag_id="ecommerce_batch_pipeline", 
+    dag_id="ecommerce_batch_pipeline",
+    default_args=default_args,
     start_date=datetime(2026, 5, 7),
-    schedule=None, 
+    schedule=None,
     catchup=False,
+    max_active_runs=1,
     tags=["ecommerce", "spark", "batch"],
 ) as dag:
 
-    build_fact_order_item = BashOperator(
-        task_id="build_fact_order_item",
-        bash_command=BASE_CMD.format(
+    with TaskGroup("silver_layer") as silver_layer:
+
+        build_fact_order_item = SparkSubmitOperator(
+            task_id="build_fact_order_item",
+            application="/app/spark/batch/build_fact_order_item.py",
+            conn_id="spark_default",
             packages=SPARK_PACKAGES,
-            script="build_fact_order_item.py",
-        ),
-    )
+            verbose=True,
+        )
 
-    build_fact_delivery = BashOperator(
-        task_id="build_fact_delivery",
-        bash_command=BASE_CMD.format(
+        build_fact_delivery = SparkSubmitOperator(
+            task_id="build_fact_delivery",
+            application="/app/spark/batch/build_fact_delivery.py",
+            conn_id="spark_default",
             packages=SPARK_PACKAGES,
-            script="build_fact_delivery.py",
-        ),
-    )
+            verbose=True,
+        )
 
-    build_fact_review = BashOperator(
-        task_id="build_fact_review",
-        bash_command=BASE_CMD.format(
+        build_fact_review = SparkSubmitOperator(
+            task_id="build_fact_review",
+            application="/app/spark/batch/build_fact_review.py",
+            conn_id="spark_default",
             packages=SPARK_PACKAGES,
-            script="build_fact_review.py",
-        ),
-    )
+            verbose=True,
+        )
 
-    check_silver_tables = BashOperator(
-        task_id="check_silver_tables",
-        bash_command=BASE_CMD.format(
+    with TaskGroup("validation_layer") as validation_layer:
+
+        check_silver_tables = SparkSubmitOperator(
+            task_id="check_silver_tables",
+            application="/app/spark/batch/check_silver_tables.py",
+            conn_id="spark_default",
             packages=SPARK_PACKAGES,
-            script="check_silver_tables.py",
-        ),
-    )
+            verbose=True,
+        )
 
-    build_gold_marts = BashOperator(
-        task_id="build_gold_marts",
-        bash_command=BASE_CMD.format(
+    with TaskGroup("gold_layer") as gold_layer:
+
+        build_gold_marts = SparkSubmitOperator(
+            task_id="build_gold_marts",
+            application="/app/spark/batch/build_gold_marts.py",
+            conn_id="spark_default",
             packages=SPARK_PACKAGES,
-            script="build_gold_marts.py",
-        ),
-    )
+            verbose=True,
+        )
 
-    [
-        build_fact_order_item,
-        build_fact_delivery,
-        build_fact_review,
-    ] >> check_silver_tables >> build_gold_marts
-
-# 태스크 그룹 추가
+    silver_layer >> validation_layer >> gold_layer
